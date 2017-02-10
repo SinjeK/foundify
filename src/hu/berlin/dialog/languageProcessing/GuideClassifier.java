f3d9625f863fb0d9ec19a01827d86c9eda4db4e1
geniegeist <vduc.nguyen@icloud.com>
2017-02-10 06:53:49 +0100

    Added Guide Classifier

--- src/hu/berlin/dialog/languageProcessing/GuideClassifier.java
+++ src/hu/berlin/dialog/languageProcessing/GuideClassifier.java
@@ -1,5 +1,7 @@
 package hu.berlin.dialog.languageProcessing;
 
+import com.ibm.watson.developer_cloud.natural_language_classifier.v1.NaturalLanguageClassifier;
+
 /**
  * Created by Duc on 09.02.17.
  */
@@ -7,6 +9,16 @@ public class GuideClassifier implements Classifier {
 
     public enum GuideCategory implements Category {
         UNSPECIFIED
+    }
+
+    // change this if a new classifier is used
+    final private static String IDENTIFIER = "f5b432x172-nlc-2044";
+    private NaturalLanguageClassifier classifier;
+
+    public GuideClassifier() {
+        super();
+        this.classifier = new NaturalLanguageClassifier();
+        this.classifier.setUsernameAndPassword("b762e2a2-f1f5-4b2a-9f91-e089c81048b5", "BA2vp3JSoRhi");
     }
 
     public GuideCategory classify(String input) {
